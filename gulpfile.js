7bd2d760e166148a88ee1b3e54a79d011f574ab7
rmcoladela@gmail.com <rmcoladela@gmail.com>
2016-12-13 19:44:14 -0200

    gulp tasks and watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-var gulp = require('gulp');
-
+var gulp = require('gulp') ,
+watch = require("gulp-watch")
+;
 gulp.task('default',function(){
     console.log('default task');
-})
+});
+
+gulp.task('html', function(){
+console.log("imagine");
+});
+
+gulp.task('styles', function(){
+console.log("styles");
+});
+
+
+gulp.task('watch', function(){
+    console.log('watch');
+    watch("./app/index.html",function(){
+        gulp.start("html");
+    });
+
+      watch("./app/assets/styles/**/*.css",function(){
+        gulp.start("styles");
+    });
+});
+
+
+
